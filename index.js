112d610f060d6ef23a73727e647a3ac7c3dc1604
Connor Minton <connor.c.minton@gmail.com>
2019-05-02 16:36:30 -0400

    implement earn command

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 #!/usr/bin/env node
-console.log('hello world');
+
+const argparse = require('argparse');
+const path = require('path');
+const repos = require('./lib/yaml-repo');
+
+main();
+
+async function main() {
+  const cli = new argparse.ArgumentParser({
+    version: '0.0.0',
+    addHelp: true,
+    description: 'A rewards system that helps you reinforce your good habits!'
+  });
+
+  const subparsers = cli.addSubparsers({ dest: 'cliCommand', help: 'sub-commands' });
+
+  const commands = {
+    earn: require('./lib/commands/earn')(subparsers),
+    redeem: require('./lib/commands/redeem')(subparsers),
+    view: require('./lib/commands/view')(subparsers)
+  };
+
+  const args = cli.parseArgs();
+
+  let action;
+  try {
+    action = require(path.resolve('lib', 'actions', args.cliCommand));
+  }
+  catch (e) {
+    cli.error('could not find an action for the sub-command');
+  }
+
+  await action(args, commands[args.cliCommand]);
+
+  repos.saveAll();
+}
